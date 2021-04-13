6a4069e01f9895ad7135886003589c232db6485e
Boban Sugareski <boban.s@slicelife.com>
2021-04-13 20:22:11 +0200

    Add mailer feature

--- lecture-1/app.js
+++ lecture-1/app.js
@@ -4,6 +4,8 @@ const router = require('./router');
 
 app.use('/', router);
 
+console.log('hehehe');
+
 app.listen('3000', (error) => {
   if (error) {
     return console.log('Error happened while starting the app on port 3000: ', error);
