6046df50fb6ae0fa1aaa1cb895ac64dec0d13000
Hristijan <hristijanpetlovski@gmail.com>
2021-04-13 21:25:35 +0200

    Add model and routes for User

--- lecture-1/app.js
+++ lecture-1/app.js
@@ -1,13 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const router = require('./router');
+const router = require("./router");
+const mongoose = require("mongoose");
+app.use(express.json());
 
-app.use('/', router);
+mongoose.connect("mongodb://localhost/healthcareapp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+app.use("/", router);
 
+<<<<<<< HEAD
 console.log('hehehe');
 app.listen('3000', (error) => {
+=======
+app.listen("3000", (error) => {
+>>>>>>> da5df71... Add model and routes for User
   if (error) {
-    return console.log('Error happened while starting the app on port 3000: ', error);
+    return console.log(
+      "Error happened while starting the app on port 3000: ",
+      error
+    );
   }
-  console.log('Application successfully started on port 3000');
+  console.log("Application successfully started on port 3000");
 });
