c7a88f7c74e6284424e571dc5d643d7ba66b4cf5
Goran94mkd <goki_bituse@hotmail.com>
2021-05-11 03:41:18 +0200

    Follow - Unfollow users

---
+++ project/services/auth/routers/users.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../../../controllers/users');
+
+router.get('/', controller.fetchAll)
+      .get('/:id', controller.fetchOne)
+      .put('/:id/follow', controller.follow)
+      .put('/:id/unfollow', controller.unfollow)
+
+
+module.exports = router;
+
+
+
+
--- project/services/auth/routers/v1.js
+++ project/services/auth/routers/v1.js
@@ -4,8 +4,8 @@ const controller = require('../../../controllers/auth');
 
 router.post('/register', controller.register)
       .post('/login', controller.login)
+      .post('/logout', controller.logout)
       .get('/refresh-token', controller.refreshToken)
-      .get('/users', controller.fetchAll)
-      .get('/users/:id', controller.fetchOne)
+
 
 module.exports = router;
